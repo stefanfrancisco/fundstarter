6a9a26e0e1eeb3d81e708a4d5da68e90ba7acbbf
Ubuntu <ubuntu@ip-172-31-18-220.us-west-2.compute.internal>
2016-10-03 18:12:07 +0000

    make it better

--- index2.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'); 
-var fs = require("fs"); 
-var buf = fs.readFileSync('public/index.html', "utf8"); 
-
-http.createServer(function(request, response) { 
-	contentType = 'text/html'; 
-	response.writeHead(200, {'Content-Type' : contentType}); 
-	response.write(buf); 
-}).listen(8080);
