cb7c7ba8bf332c72365c2880a87d84108db7798e
Ubuntu <ubuntu@ip-172-31-18-220.us-west-2.compute.internal>
2016-10-03 18:28:33 +0000

    Commit

---
+++ index1_B.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require("fs");
+var buf = fs.readFileSync('public/index.html', "utf8");
+
+http.createServer(function(request, response) {
+        contentType = 'text/html';
+        response.writeHead(200, {'Content-Type' : contentType});
+        response.write(buf);
+}).listen(8080);
